7a7c822ffa4e03787b728f25592dbc16ef11fb62
Barry Newman <bnewman@saveology.com>
2011-03-24 13:54:24 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyApp::Application.config.secret_token = 'f2cb322559b1b3296d811a60ace7c79be868ae253581e094664be42881df1cada9b68f49a2eae4268dc0232424bd928d1f02237b4a4e38099d86de7cea44add2'
